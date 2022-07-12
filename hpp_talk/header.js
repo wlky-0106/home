4fa8b93c80da6494174bab8c705c8db49c42481e
wlky <wlky080106@qq.com>
2022-07-12 08:24:15 +0800

    no message

---
+++ hpp_talk/header.js
@@ -0,0 +1,19 @@
+document.write('<!-----------  公共js和css start----------->');
+document.write('' +
+    '<link rel="stylesheet" href="https://fastly.jsdelivr.net/gh/wlky-0106/texiao/source/css/shubiao.css">\n' +
+    '<link rel="stylesheet" href="https://at.alicdn.com/t/font_3441865_engdww6vg1t.css" type="text/css">\n' +
+//     '<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?8c0854c37b23b617c441acb65529fca1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>\n' +
+    '<link rel="apple-touch-icon" sizes="76x76" href="source/img/logo.png">\n' +
+    '<link rel="shortcut icon" href="source/img/logo.png" type="image/x-png">\n' +
+    '<link rel="stylesheet" href="./mystyle.css">\n' +
+    '<!--\n' +
+    'Hi, It\'s TuHome. This is a good homepage theme.\n' +
+    'Github: https://github.com/ye-tutu/TuHome\n' +
+    'Gitee: https://gitee.com/ye-tutu/TuHome\n' +
+    '-->' +
+    '');
+
+
+
+
+
